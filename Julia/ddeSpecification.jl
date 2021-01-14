08b5e59c1f040f23181fcb1e7cff448a359222c4
elizabethjmohr <elizabethjmohr@gmail.com>
2021-01-13 19:58:21 -0700

    Put simulation code into a function. Changed variables to greek letters.

--- Julia/ddeSpecification.jl
+++ Julia/ddeSpecification.jl
@@ -2,42 +2,37 @@ using DifferentialEquations
 using QuadGK
 using Plots
 
-# Specify parameters
-tauMin = 1.00e-3
-tauMax = 1.00e3
-a = 1.6
-k = 0.00
-S = 1.00
-q = S/quadgk(tau -> (tauMax^(-a+1.00) - tau^(-a+1.00)- (tauMax^(-a)*(tauMax-tau)*(-a+1.00)))/(tauMax^(-a+1.00) - tauMin^(-a+1.00)- (tauMax^(-a)*(tauMax-tauMin)*(-a+1.00))), tauMin, tauMax)[1]
-p = [tauMin, tauMax, a, k, q]
-
-# Specify initial condition
-u0 = [4.000]
-
-# Specify condition before simulation start
-h(p,t) = 0.000
-
 # Define model function
 function sam_model!(du,u,h,p,t)
-  tauMin, tauMax, a, k, q = p
-  if(tauMin<t<tauMax)
-    integral, error = quadgk(tau -> ((tau^(-a) - tauMax^(-a))*(-a+1)/(tauMax^(-a+1)-tauMin^(-a+1)-tauMax^(-a)*(tauMax - tauMin)*(1-a))) * h(p,t-tau)[1]*exp(-k*tau), tauMin, t, tauMax)
+  τ₋, τ₊, α, k, q = p
+  if(τ₋<t<τ₊)
+    integral, error = quadgk(τ -> ((τ^(-α) - τ₊^(-α))*(-α+1)/(τ₊^(-α+1)-τ₋^(-α+1)-τ₊^(-α)*(τ₊ - τ₋)*(1-α))) * h(p,t-τ)[1]*exp(-k*τ),τ₋, t, τ₊)
   else
-    integral, error = quadgk(tau -> ((tau^(-a) - tauMax^(-a))*(-a+1)/(tauMax^(-a+1)-tauMin^(-a+1)-tauMax^(-a)*(tauMax - tauMin)*(1-a))) * h(p,t-tau)[1]*exp(-k*tau), tauMin, tauMax)
+    integral, error = quadgk(τ -> ((τ^(-α) - τ₊^(-α))*(-α+1)/(τ₊^(-α+1)-τ₋^(-α+1)-τ₊^(-α)*(τ₊- τ₋)*(1-α))) * h(p,t-τ)[1]*exp(-k*τ), τ₋, τ₊)
   end
   du[1] = q*(integral - u[1])
 end
-# Specify simulation time
-tspan = (0.00, 1.00e3)
 
-# Build the DDE problem
-prob = DDEProblem(sam_model!, u0, h, tspan, p)
+function simulateSAM(τ₋ = 1e-3,τ₊ = 1e3, α = 1.6, k = 0.0, S = 1.0, hist = 0.0, C₀ = [4.0], t₀ = 0.0, ⏰ = 1e3)
+  # Calculate hyporheic exchange (volume per time)
+  q = S/quadgk(τ -> (τ₊^(-α+1.00) - τ^(-α+1.00)- (τ₊^(-α)*(τ₊-τ)*(-α+1.00)))/(τ₊^(-α+1.00) - τ₋^(-α+1.00)- (τ₊^(-α)*(τ₊-τ₋)*(-α+1.00))), τ₋, τ₊)[1]
+  # Put model parameters into a vector
+  p = [τ₋, τ₊, α, k, q]
+  # Make a tuple containing the start and end times of the simulation
+  tspan = (t₀, ⏰)
+  # Specify condition before simulation start
+  h(p,t) = hist
+  # Build the DDE problem
+  prob = DDEProblem(sam_model!, C₀, h, tspan, p)
+  # Specify algorithm
+  alg = MethodOfSteps(RK4())
+  # Solve problem
+  sol = solve(prob, alg, reltol = 1e-6)
+  return(sol)
+end
 
-# Specify algorithm
-alg = MethodOfSteps(RK4())
-
-# Solve problem
-sol = solve(prob, alg)
+# Solve with default parameters
+solution = simulateSAM()
 
 # Plot solution
-plot(sol)
+plot(solution)
